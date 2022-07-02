cd1b0be6ae93a9a9c73c37ca40322b18905d5d22
Peter Oberzier <peter.oberzier@melpro.de>
2022-07-02 08:30:10 +0200

    all tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,40 @@
-const removeFromArray = function() {
+'use strict';
+
+const removeFromArray = function(inputArray, ...toRemoveItems) {
+
+    // split toRemoveItems into two separate arrays:
+    // 1 will contain all numbers and will be sorted 
+    // 2 will contain all strings
+
+    let toRemoveItemsNumbers = [];
+    let toRemoveItemsStrings = [];
+    let dummy = 0;
+
+    for (let i = 0; i < toRemoveItems.length; i++) {
+        typeof(toRemoveItems[i]) == 'number' ? toRemoveItemsNumbers.push(toRemoveItems[i]) : dummy = 0;
+        typeof(toRemoveItems[i]) == 'string' ? toRemoveItemsStrings.push(toRemoveItems[i]) : dummy = 0;
+    }
+
+    toRemoveItemsNumbers = toRemoveItemsNumbers.sort();
+    toRemoveItemsNumbers = toRemoveItemsNumbers.reverse();
+
+    // first remove all numbered items
+    for (let i = 0; i < toRemoveItemsNumbers.length; i++) {
+        inputArray.splice(toRemoveItemsNumbers[i] - 1, 1);
+    }
+    
+    // now remove all strings
+    let toRemoveItem = '';
+    let startPosition = 0;
+    for (let i = 0; i < toRemoveItemsStrings.length; i++) {
+        toRemoveItem = toRemoveItemsStrings[i];
+        while (inputArray.includes(toRemoveItem)) {
+            startPosition = inputArray.indexOf(toRemoveItem);
+            inputArray.splice(startPosition, 1);
+        }
+    }
+
+    return inputArray;
 
 };
 
