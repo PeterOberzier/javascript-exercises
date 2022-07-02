6ba683dae60f2f3328ef2c976b0dcc4e80e240d5
Peter Oberzier <peter.oberzier@melpro.de>
2022-07-02 09:05:40 +0200

    refactored code with shorter if-statements and for-loops

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,37 +3,36 @@
 const removeFromArray = function(inputArray, ...toRemoveItems) {
 
     // split toRemoveItems into two separate arrays:
-    // 1 will contain all numbers and will be sorted 
+    // 1 will contain all numbers and
     // 2 will contain all strings
 
     let toRemoveItemsNumbers = [];
     let toRemoveItemsStrings = [];
-    let dummy = 0;
 
-    for (let i = 0; i < toRemoveItems.length; i++) {
-        typeof(toRemoveItems[i]) == 'number' ? toRemoveItemsNumbers.push(toRemoveItems[i]) : dummy = 0;
-        typeof(toRemoveItems[i]) == 'string' ? toRemoveItemsStrings.push(toRemoveItems[i]) : dummy = 0;
+    for (const toRemoveItem of toRemoveItems) {
+        if (typeof(toRemoveItem) == 'number') toRemoveItemsNumbers.push(toRemoveItem);
+        if (typeof(toRemoveItem) == 'string') toRemoveItemsStrings.push(toRemoveItem);
     }
 
+    // sort the array with numbers descending
     toRemoveItemsNumbers = toRemoveItemsNumbers.sort();
     toRemoveItemsNumbers = toRemoveItemsNumbers.reverse();
 
     // first remove all numbered items
-    for (let i = 0; i < toRemoveItemsNumbers.length; i++) {
-        inputArray.splice(toRemoveItemsNumbers[i] - 1, 1);
+    for (const toRemoveItem of toRemoveItemsNumbers) {
+        inputArray.splice(toRemoveItem - 1, 1);
     }
     
     // now remove all strings
-    let toRemoveItem = '';
     let startPosition = 0;
-    for (let i = 0; i < toRemoveItemsStrings.length; i++) {
-        toRemoveItem = toRemoveItemsStrings[i];
+    for (const toRemoveItem of toRemoveItemsStrings) {
         while (inputArray.includes(toRemoveItem)) {
             startPosition = inputArray.indexOf(toRemoveItem);
             inputArray.splice(startPosition, 1);
         }
     }
 
+    // return remaining array
     return inputArray;
 
 };
