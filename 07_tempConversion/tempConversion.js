6d43b1768b67919b20f076ebedb61a46a8dbdd0b
Peter Oberzier <peter.oberzier@melpro.de>
2022-07-03 13:35:53 +0200

    all tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
+'use strict';
 
+const ftoc = function(inputValue) {
+  let returnValue = (inputValue - 32) * 5 / 9;
+  return (parseInt(returnValue, 10) === returnValue) ?  returnValue : parseFloat(returnValue.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(inputValue) {
+  let returnValue = (inputValue * 9 / 5) + 32;
+  return (parseInt(returnValue, 10) === returnValue) ?  returnValue : parseFloat(returnValue.toFixed(1));
 };
 
 // Do not edit below this line
